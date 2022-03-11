133d1f8c6760dee334ed42c59c6a04ab7c7fdb22
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2022-03-12 00:14:27 +0530

    Main

--- Route/routers.js
+++ Route/routers.js
@@ -16,14 +16,14 @@ router.post("/api/v1/openai", async (req, res) => {
   try {
     console.log("started to generate AI Completion");
     const response = await openai.createCompletion("text-davinci-001", {
-      prompt: `Generate an Email to send from sender to receiver according to the given context\n\nGiven context: ${data}\nGenerated Email:`,
+      prompt: `Turn the given points into a meaningful email.\n\nGiven points: ${data}\nGenerated Email:`,
       temperature: 1,
       max_tokens: 250,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
     });
-    console.log(response.data.choices[0].text);
+    res.send(response.data.choices[0].text);
     console.log("successfully Generated AI Completion");
   } catch (error) {
     console.log("error while generating AI Completion --> " + error);
