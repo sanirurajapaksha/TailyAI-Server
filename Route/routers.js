4e4c8ba56c38769d5d9b3ae56c68355b02a5fc72
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2022-03-16 19:56:02 +0530

    Master

--- Route/routers.js
+++ Route/routers.js
@@ -1,8 +1,8 @@
-import exress from "express";
+import express from "express";
 import dotenv from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
 
-const router = exress.Router();
+const router = express.Router();
 dotenv.config();
 
 const configuration = new Configuration({
@@ -13,19 +13,55 @@ const openai = new OpenAIApi(configuration);
 
 router.post("/api/v1/openai", async (req, res) => {
   const data = req.body.text;
+  const prompt = `Turn the given points into a meaningful and polite email.\n\nGiven points: ${data}\nGenerated Email:`;
   try {
-    console.log("started to generate AI Completion");
-    const response = await openai.createCompletion("text-davinci-001", {
-      prompt: `Turn the given points into a meaningful email.\n\nGiven points: ${data}\nGenerated Email:`,
-      temperature: 1,
-      max_tokens: 250,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-    res.send(response.data.choices[0].text);
+    const getTheFilterResponse = async () => {
+      const filter_response = await openai.createCompletion(
+        "content-filter-alpha",
+        {
+          prompt: `<|endoftext|>${prompt}\n--\nLabel:`,
+          temperature: 0,
+          max_tokens: 1,
+          top_p: 0,
+          logprobs: 10,
+        }
+      );
+      return filter_response;
+    };
+
+    const getMainResponse = async () => {
+      const main_response = await openai.createCompletion("text-davinci-001", {
+        prompt: prompt,
+        temperature: 1,
+        max_tokens: 250,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+      return main_response;
+    };
+
+    const toxic_threshold = -0.355;
+    const content_filter_value = (await getTheFilterResponse()).data.choices[0]
+      .text;
+    const longprob_value = (await getTheFilterResponse()).data.choices[0]
+      .logprobs.top_logprobs[0][content_filter_value];
+
+    if (content_filter_value == "0" || content_filter_value == "1") {
+      res.send((await getMainResponse()).data.choices[0].text);
+    } else if (
+      content_filter_value == "2" &&
+      longprob_value < toxic_threshold
+    ) {
+      res.send("regenerate");
+    } else if (
+      content_filter_value == "2" &&
+      longprob_value > toxic_threshold
+    ) {
+      res.send("toxic");
+    }
   } catch (error) {
-    console.log("error while generating AI Completion --> " + error);
+    console.log("" + error);
   }
 });
 
