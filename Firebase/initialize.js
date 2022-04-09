444e21acb20e4f7bc94fec069c39ca5cfc1eb83c
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2022-04-09 23:10:49 +0530

    Main

--- Firebase/initialize.js
+++ Firebase/initialize.js
@@ -1,6 +1,7 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { createRequire } from "module"; // Bring in the ability to create the 'require' method
+
 const require = createRequire(import.meta.url); // construct the require method
 const service = require("./service-account.json");
 
