b5746294552059def96325a21b90a5613057df09
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2022-04-28 20:54:54 +0530

    main

--- Route/routers.js
+++ Route/routers.js
@@ -23,7 +23,7 @@ router.post("/api/v1/openai", async (req, res) => {
   const snapshot = db.collection("users").doc(cache.get(req.body.email));
   const doc = await snapshot.get();
 
-  const prompt = `Turn the given points into a meaningful and polite email.\n\nGiven points: ${data}\nGenerated Email:`;
+  const prompt = `Turn the given points into a meaningful, polite and formal email.\n\nGiven points: ${data}\nGenerated Email:`;
   try {
     const getTheFilterResponse = async () => {
       const filter_response = await openai.createCompletion(
@@ -316,6 +316,23 @@ router.post("/api/v1/firebase/auth", async (req, res) => {
     } else {
       cache.put(req.body.email, req.body.uid);
       console.log("auth_uid set: " + cache.get(req.body.email));
+
+      // const snapshot = db.collection("auth_ids").doc(req.body.email);
+
+      // const result = cache.exportJson();
+      // const json_object = JSON.parse(result);
+
+      // const formatted_json_object = {};
+
+      // for (var i in json_object) {
+      //   for (var x in json_object[i]) {
+      //     if (json_object[i][x] !== "NaN") {
+      //       formatted_json_object[i] = json_object[i][x];
+      //     }
+      //   }
+      // }
+
+      // await snapshot.set(formatted_json_object, { merge: true });
     }
   } catch (error) {
     console.log("" + error);
